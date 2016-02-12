a255f6a0f4944ba508a3db55023462b2c25d519c
lucasvieira20 <web.lucasvieira@gmail.com>
2016-02-11 23:59:07 -0200

    Configurando primeira rota

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'connectedin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('perfis.urls'))
+    url(r'^$', 'perfis.views.index')
 )
