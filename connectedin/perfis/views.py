28ee5972d4da215ea39ef73cd6647579a7e7d14a
lucasvieira20 <web.lucasvieira@gmail.com>
2016-02-12 00:28:32 -0200

    Renderizando perfil na view, criando classe de Perfil

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 # Create your views here.
 
 def index(request):
 	return render(request, 'index.html')
 
+def exibir(request, perfil_id):
+	perfil = Perfil()
+
+	if perfil_id == '1':
+		perfil = Perfil("Flavio Almeida","flavio@flavio.com.br","22222","Caelum")
+
+	if perfil_id == "2":
+		perfil = Perfil("Romulo Henrique","romulo@romulo.com.br","1233443","Caelum")
+		
+	return render(request, 'perfil.html', {"perfil" : perfil})
