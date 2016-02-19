26c17828bc5baa4993b561af83b59bf03991c895
lucasvieira20 <web.lucasvieira@gmail.com>
2016-02-19 00:29:10 -0200

    Convidando Perfil

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from perfis import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'perfis.views.index', name="index"),
-    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name="exibir")
+    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name="exibir"),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar', views.convidar, name="convidar")
 )
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from perfis.models import Perfil
 # Create your views here.
 
@@ -9,3 +10,12 @@ def exibir(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
 	
 	return render(request, 'perfil.html', {"perfil" : perfil})
+
+def convidar(request, perfil_id):
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	perfil_logado.convidar(perfil_a_convidar)
+	return redirect('index')
+
+def get_perfil_logado(request):
+	return Perfil.objects.get(id=1)
