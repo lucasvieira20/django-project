4dec1998883a41c299fcaff990f6f3d04f7436a4
lucasvieira20 <web.lucasvieira@gmail.com>
2016-03-10 00:39:36 -0300

    Autenticação e validação de form

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from perfis.models import Perfil, Convite
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
 	return render(request, 'index.html', { 'perfis' : Perfil.objects.all(), 
 		'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
-    perfil = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    ja_eh_contato = perfil in perfil_logado.contatos.all()
-    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_eh_contato' : ja_eh_contato})
+	
+	perfil = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	ja_eh_contato = perfil in perfil_logado.contatos.all()
+	return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 
+		'ja_eh_contato' : ja_eh_contato})
 
+@login_required
 def convidar(request, perfil_id):
 
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
@@ -21,10 +27,12 @@ def convidar(request, perfil_id):
 
 	return redirect('index');
 
+@login_required
+def get_perfil_logado(request):
+	return request.user.perfil 
+
+@login_required
 def aceitar(request, convite_id):
 	convite = Convite.objects.get(id=convite_id)
 	convite.aceitar()
-	return render('index')
-
-def get_perfil_logado(request):
-	return Perfil.objects.get(id=1) 
+	return redirect('index')
